d400c8e93a03ddb15bb349842083474ad0a1873e
Simon Bui <sibui@ucsd.edu>
2015-02-16 00:42:21 +0000

    finishing up the lab

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,11 @@
 var models = require('../models');
 
-exports.projectInfo = function(req, res) { 
+exports.projectInfo = function(req, res) {
   var projectID = req.params.id;
+  console.log(projectID);
+  models.Project
+  .find({"_id":projectID})
+  .exec(afterQuery);
 
   // query for the specific project and
   // call the following callback
@@ -14,15 +18,24 @@ exports.projectInfo = function(req, res) { 
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
-  console.log(form_data);
-
+  console.log(form_data.project_title);
+  
+  var newProject = new models.Project({
+	  "title": form_data.project_title,
+	  "date": form_data.date,
+	  "summary": form_data.summary,
+	  "image": form_data.image_url
+  });
+  newProject.save(res.send());
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
-
+  models.Project
+	.find({"_id":projectID})
+	.exec(res.send());
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
